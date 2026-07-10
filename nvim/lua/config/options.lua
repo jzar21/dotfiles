db1273dc1b654489ce2d116bb171bc36db6d6fa5
jzar21 <juanfmontilla21@gmail.com>
2026-07-10 08:55:09 +0200

    feat: dunst y alguna config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,8 +3,8 @@
 -- Add any additional options here
 
 vim.opt.colorcolumn = "80,120"
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
+vim.opt.scrolloff = 16
+vim.opt.sidescrolloff = 16
 vim.opt.smartcase = true
 vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
